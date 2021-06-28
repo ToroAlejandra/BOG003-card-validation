58a5c61f6381d4f4da2fc7cf19ccf98064885746
ToroAlejandra <jessicaalejandratc@gmail.com>
2021-06-28 10:34:55 -0500

    Se añaden funciones isValid() y maskify() al codigo

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-
-import validator from './validator.js';
+import validator from "./validator.js";
 
 console.log(validator);
 
+console.log(document.getElementById("numberTc").value);
 
-document.getElementById("botonalerta").addEventListener("click", validator.alerta)
+document.getElementById("botonalerta").addEventListener("click", () => {
+  if (document.getElementById("numberTc").value !== "") {
+    validator.isValid(document.getElementById("numberTc").value);
+    validator.maskify(document.getElementById("numberTc").value);
+  } else {
+    alert("No has ingresado valor.. ");
+  }
+});
 
-    
-
-  
-  
+document.getElementById("formTc").addEventListener("click", function (event) {
+  event.preventDefault();
+});
--- src/validator.js
+++ src/validator.js
@@ -1,8 +1,56 @@
 const validator = {
-   alerta:()=>{
-alert("Hola");
+  isValid: (creditCardNumber) => {
+    let creditCardNumberInverse = [];
+    let splitNumber = [];
+    let sum = 0;
+
+    creditCardNumber = creditCardNumber.split("");
+
+    for (let i = 0; i < creditCardNumber.length; i++) {
+      creditCardNumberInverse[i] =
+        creditCardNumber[creditCardNumber.length - (i + 1)];
+      if (i % 2 != 0) {
+        creditCardNumberInverse[i] = creditCardNumberInverse[i] * 2;
+        if (creditCardNumberInverse[i] >= 10) {
+          splitNumber = creditCardNumberInverse[i].toString().split("");
+          creditCardNumberInverse[i] =
+            parseInt(splitNumber[0]) + parseInt(splitNumber[1]);
+        }
+      }
+      sum = sum + parseInt(creditCardNumberInverse[i]);
+    }
+    console.log(creditCardNumberInverse);
+    let res = sum % 10 == 0 ? true : false;
+
+    return res;
+  },
   
-  }};
+  maskify: (creditCardNumber) => {
+    let mask = [];
+    let creditCardNumberMask = [];
+
+    creditCardNumber = creditCardNumber.split("");
+
+    for (let i = 0; i < creditCardNumber.length - 4; i++) {
+      if (creditCardNumber.length > 4) {
+        mask[i] = "#";
+      } else {
+        mask[i] = creditCardNumber[i];
+      }
+    }
+
+    for (let i = 0; i < creditCardNumber.length; i++) {
+      if (mask[i] === "" || mask[i] === null || mask[i] === undefined) {
+        creditCardNumberMask[i] = creditCardNumber[i];
+      } else {
+        creditCardNumberMask[i] = mask[i];
+      }
+    }
+
+    creditCardNumber = creditCardNumberMask.join("");
+    console.log(creditCardNumber);
+    return creditCardNumber;
+  },
+};
 
 export default validator;
-
